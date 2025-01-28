f258dbce1a835956ae7dbd2798d71a97043c29af
dianMcdawn <patricio.a.henriquez>
2025-01-28 14:36:21 -0300

    sync

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -1,49 +1,4 @@
-//Fetching JSON to be displayed
-const url = 'data/membership.json';
-
-async function getMembershipData() {
-	const response = await fetch(url);
-	const data = await response.json();
-	displayItems(data.membership);
-}
-
-getMembershipData();
-
-/* ******************************************************************* */
-const membershipCards = document.querySelector('#membership');
-const membershipDetails = document.querySelector('#memberships-desc');
-const membershipDetailsTitle = document.querySelector('#memberships-desc h2');
-const membershipDetailsInfo = document.querySelector('#memberships-desc p');
-const membershipDetailsClose = document.querySelector('#memberships-desc #dialog-close');
-membershipDetailsClose.addEventListener("click", () => membershipDetails.close())
-
-function displayItems(data){
-    data.forEach(x => {
-        const card = document.createElement('div');
-        card.classList.add("membership-section");
-		card.setAttribute("id", `member${x.id}`);
-        const fullName = document.createElement('h3');
-        fullName.textContent = `${x.membershipName} Membership`;
-
-        const buttonRead = document.createElement('button');
-        buttonRead.textContent = `Read more`;
-        buttonRead.classList.add("membership-link");
-        buttonRead.addEventListener('click', () => showStuff(x));
-
-        //Appending data to main cards
-        card.appendChild(fullName);
-        card.appendChild(buttonRead);
-        membershipCards.appendChild(card);
-    })
-}
-
-function showStuff(x){
-    membershipDetailsTitle.innerHTML = x.membershipName;
-    membershipDetailsInfo.innerHTML = x.description;
-    membershipDetails.showModal();
-}
-
-/* ******************************************************************* */
+/* TimeStamp for hidden input */
 
 const todayMark = new Date();
 const yearMark = todayMark.getFullYear();
@@ -57,3 +12,51 @@ let timeStamp = `${yearMark}-${monthMark}-${dayMark} ${hourMark}:${minuteMark}:$
 
 const dateMark = document.querySelector("#timestamp");
 dateMark.value = timeStamp;
+
+/* Using json for membership */
+//Fetching JSON to be displayed
+const url = 'data/membership.json';
+
+async function getMembershipData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    displayItems(data.membership);
+}
+
+getMembershipData();
+
+/* ******************************************************************* */
+const membershipCards = document.querySelector('#membership');
+const membershipDetails = document.querySelector('#membership-desc');
+const membershipDetailsTitle = document.querySelector('#membership-desc h2');
+const membershipDetailsInfo = document.querySelector('#membership-desc p');
+const membershipDetailsClose = document.querySelector('#membership-desc #dialog-close');
+membershipDetailsClose.addEventListener("click", () => membershipDetails.close())
+
+function displayItems(data) {
+    data.forEach(x => {
+        let card = document.createElement('div');
+        card.classList.add("membership-section");
+        card.setAttribute("id", `member${x.id}`);
+        let fullName = document.createElement('h3');
+        fullName.textContent = `${x.membershipName} Membership`;
+
+        let buttonRead = document.createElement('button');
+        buttonRead.textContent = `Read more`;
+        buttonRead.classList.add("membership-link");
+        buttonRead.addEventListener('click', () => showStuff(x));
+
+        //Appending data to main cards
+        card.appendChild(fullName);
+        card.appendChild(buttonRead);
+        membershipCards.appendChild(card);
+    })
+}
+
+function showStuff(x) {
+    membershipDetailsTitle.innerHTML = `${x.membershipName} Membership`;
+    membershipDetailsInfo.innerHTML = x.description;
+    membershipDetails.showModal();
+}
+
+/* ******************************************************************* */
