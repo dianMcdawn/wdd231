8ae07ef02895b65bd13ecd628be02c919c365172
dianMcdawn <patricio.a.henriquez>
2025-02-05 11:41:56 -0300

    sync

--- chamber/scripts/guest-check.js
+++ chamber/scripts/guest-check.js
@@ -2,6 +2,7 @@
 
 //Timestamp
 const todayMark = new Date();
+const msToDays = 86400000;
 
 let yearMark = todayMark.getFullYear();
 let monthMark = todayMark.getMonth();
@@ -17,6 +18,7 @@ const guestDisplay = document.querySelector("#guest-visitor");
 
 // Get the stored VALUE for the emailsCount-ls KEY in localStorage if it exists. If the emailsCount KEY is missing, then assign 0 to the emailsCount variable.
 let lastVisit = Date(window.localStorage.getItem("lastVisit-ls")) || now;
+let visitCount = Number(window.localStorage.getItem("visitCount-ls")) || 0;
 
 let dateSegment = lastVisit.split(" GMT");
 let dateStamp = lastVisit.split(" ");
@@ -49,15 +51,20 @@ let diff = now.getTime() - date.getTime();
 //1000 miliseconds = 1 second; 60 seconds = 1 minute; 60 minutes = 1 hour; 24 hours = 1 day / 86.400.000 miliseconds = 1 day
 let message = "";
 
-if (diff == 0) { message = "Welcome! Let us know if you have any questions."; }
-else if (diff < 86400000) { message = "Back so soon! Awesome!"; }
-else if (diff >= 86400000) {
+if (visitCount == 0) { message = "Welcome! Let us know if you have any questions."; }
+else if (diff < msToDays) { message = "Back so soon! Awesome!"; }
+else if (diff >= msToDays) {
     let daysDiff = diff / (1000 * 60 * 60 * 24);
     message = `You last visited ${daysDiff} days ago.`;
 }
 message = message + ` ${diff}`;
+
+//Visita
+visitCount++;
+
 // Show the reviews count on screen
-guestDisplay.textContent = `${message}`;
+guestDisplay.textContent = `${message} ${visitCount}`;
 
 // Store the new visit total into localStorage, key=emailsCount-ls
 localStorage.setItem("lastVisit-ls", now);
+localStorage.setItem("visitCount-ls", visitCount);
